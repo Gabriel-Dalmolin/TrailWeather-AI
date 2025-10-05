347c9eff68ea056eac756f0d7ce36804bf4f2a8e
Gabriel <gabrielsomacaldalmolin@gmail.com>
2025-10-05 11:28:12 -0300

    Revert "v1"
    This reverts commit 7891a7d28f771f189204305b5b7e94d7070027fd.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <BrowserRouter basename="/TrailWeather-AI">
+        <App />
+    </BrowserRouter>
+);
