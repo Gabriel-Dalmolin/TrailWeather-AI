0eda848e6acddd9a02c567b4fe09c5b4dac02f37
Gabriel <gabrielsomacaldalmolin@gmail.com>
2025-10-05 11:29:14 -0300

    reverted to older version

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-    <BrowserRouter basename="/TrailWeather-AI">
-        <App />
-    </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
